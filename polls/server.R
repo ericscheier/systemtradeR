78d9e9b2da181bbc42ef7172b24be4164a748fa5
ericscheier <eric@scheier.org>
2016-07-26 13:32:00 +0200

    add polls data and update system files for production

---
+++ polls/server.R
@@ -0,0 +1,6 @@
+
+# pick out forecast based on desired poll [feature]
+# load the functions
+# load the raw data
+# apply the function to the raw data with the correct settings (inputs)
+# return the results as a vector, or table?
