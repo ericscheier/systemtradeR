cf53f5dc332a4dbaade3d76549ae973b1d1ba1f4
ericscheier <eric@scheier.org>
2016-07-27 23:17:04 +0200

    mostly integrate quantstrat backtesting for instrument weights

--- helpers.R
+++ helpers.R
@@ -1,3 +1,17 @@
 pcchance = function(x, lag=1){
   c(diff(x,lag), rep(NA,lag))/x
 }
+
+changeFiles <- function(file){
+  file.name <- file.path(path, file)
+  existing.data <- read.csv(file.name, stringsAsFactors = FALSE, row.names="X")
+  write.csv(existing.data, file=file.name, row.names=FALSE)
+}
+
+editDirectory <- function(){
+  path <- NULL #"./data/raw/backups"
+  infiles <- dir(path =path, pattern='\\.csv$')
+  
+  lapply(infiles , changeFiles)
+  
+}
