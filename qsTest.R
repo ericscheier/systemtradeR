6f4e9f519bdc6994312be2f1f755383b217612f0
ericscheier <eric@scheier.org>
2016-07-29 00:15:34 +0200

    functional with complete update of R and packages, found some bugs

--- qsTest.R
+++ qsTest.R
@@ -6,4 +6,18 @@ qsTest <- function(){
   simulateSubsystem(pair="BTC_CLAM")
 }
 
+qmTest <- function(){
+  library(quantmod)
+  library(xts)
+  
+  getSymbols("SPY", src="google", from = "2004-01-01")
+  x1 <- SPY['2006-01/2007-12']
+  
+  x2 <- apply.monthly(x1,mean)
+  print(x2['2006-01/2007-12'])
+  
+  x3 <- as.xts(coredata(x2),order.by = as.yearmon(index(x2)))
+  print(x3['2006-01/2007-12'])
+}
+
 qsTest()
