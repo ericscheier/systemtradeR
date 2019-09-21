40cc87b4eed9160ccd5176fa924942a0ef7f6e97
virus <eric@scheier.org>
2019-09-21 14:33:22 -0400

    update folder structure

--- polls/server.R
+++
@@ -1,6 +0,0 @@
-
-# pick out forecast based on desired poll [feature]
-# load the functions
-# load the raw data
-# apply the function to the raw data with the correct settings (inputs)
-# return the results as a vector, or table?
